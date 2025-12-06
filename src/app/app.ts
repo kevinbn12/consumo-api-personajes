35601eb4a5b705a104d2deb63d48d9a63a4d87d3
1201kevBN <bohorquezkevin12@gmail.com>
2025-12-06 15:18:47 -0500

    Crear NavBar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('consumoApiPersonajes');
+export class App{
+
+  constructor(
+    private _router : Router,
+  ){
+
+  }
+
+  navigateToRickAndMortyList(){
+    this._router.navigate(['RickAndMorty'])
+  }
+  navigateToSimpsonsList(){
+    this._router.navigate(['Simpsons'])
+  }
+  navigateToDragonBallList(){
+    this._router.navigate(['DragonBall'])
+  }
 }
